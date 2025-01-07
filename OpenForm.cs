88641d5bd7433bb0905ea70e1693d37879d591a8
lvd536 <nbikiti@gmail.com>
2025-01-07 05:04:54 +0400

    Набросок для реализации

---
+++ OpenForm.cs
@@ -0,0 +1,11 @@
+﻿namespace TgBotPractice;
+using TelegramBotBase.Base;
+using TelegramBotBase.Form;
+public class OpenForm : FormBase
+{
+    
+    public override async Task Render(MessageResult message)
+    {
+        await Device.Send("OpenComand");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TgBotPractice;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        BackTask t = new BackTask();
+        await t.Start();
+    }
+}
