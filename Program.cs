56be880d4d6077f35325adf1062e6a52a91adf68
lvd536 <nbikiti@gmail.com>
2025-01-07 23:58:11 +0400

    реализация callback + onmsg

--- Program.cs
+++ Program.cs
@@ -1,22 +1,45 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-
+using TgBotPractice;
 using var cts = new CancellationTokenSource();
 var bot = new TelegramBotClient("7557004382:AAFSqf56fgYQWHvpg1VU6zGJxJ_mdaQnkTI", cancellationToken: cts.Token);
 var me = await bot.GetMe(); // Гетаем нейм бота
+WeatherCommand weatherCommand = new WeatherCommand();
 bot.OnMessage += OnMessage;
-
+bot.OnUpdate += OnCallbackQuery;
 Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
 Console.ReadLine();
 cts.Cancel(); // stop the bot
-
 // МСГ Хендл
 async Task OnMessage(Message msg, UpdateType type)
 {
     if (msg.Text is null) return;	// Если текс нулл - идет нахуй
-    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
-    // Возврат соо 
-    await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
-    
+    if (msg.Text.StartsWith('/'))
+    {
+        switch (msg.Text)
+        {
+            case "/start":
+                
+                break;
+            
+            case "/weather":
+                await weatherCommand.StartCommand(bot, msg, type);
+                break;
+        }
+    }
+    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}"); // DEBUG data
+        //await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
+}
+
+async Task OnCallbackQuery(Update update)
+{
+    if (update.Type != UpdateType.CallbackQuery) return;
+    switch (update.CallbackQuery?.Data)
+    {
+        case "weatherCall":
+            string weatherInfo = "Текущая погода: солнечно, +20°C";
+            await bot.SendMessage(update.CallbackQuery.Message.Chat.Id, weatherInfo);
+            break;
+    }
 }
---
+++ WeatherCommand.cs
@@ -0,0 +1,13 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+namespace TgBotPractice;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+public class WeatherCommand
+{
+    public async Task StartCommand(ITelegramBotClient botClient, Message msg, UpdateType type)
+    {
+        var test = new InlineKeyboardMarkup().AddButton("Weather", "weatherCall");
+        await botClient.SendMessage(msg.Chat, "Пока что в боте есть только 1 возможность - просмотр погоды", replyMarkup: test);
+    }
+}
