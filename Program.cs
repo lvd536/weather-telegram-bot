6a95b75cff2c07d01e071f241066e9b7fca31d8d
lvd536 <nbikiti@gmail.com>
2025-01-07 23:12:42 +0400

    Новый тест набросок для работы

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace TgBotPractice;
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
-public class Program
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient("7557004382:AAFSqf56fgYQWHvpg1VU6zGJxJ_mdaQnkTI", cancellationToken: cts.Token);
+var me = await bot.GetMe(); // Гетаем нейм бота
+bot.OnMessage += OnMessage;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel(); // stop the bot
+
+// МСГ Хендл
+async Task OnMessage(Message msg, UpdateType type)
 {
-    public static async Task Main()
-    {
-        BackTask t = new BackTask();
-        await t.Start();
-    }
+    if (msg.Text is null) return;	// Если текс нулл - идет нахуй
+    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+    // Возврат соо 
+    await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
+    
 }
--- obj/Debug/net9.0/TgBotPractice.AssemblyInfo.cs
+++ obj/Debug/net9.0/TgBotPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TgBotPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88641d5bd7433bb0905ea70e1693d37879d591a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TgBotPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TgBotPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
