c0b8b6418d78589a3a0a768a8fb275158118b7d6
lvd536 <nbikiti@gmail.com>
2025-01-09 01:14:54 +0400

    DB template

---
+++ DataBase/ApplicationContext.cs
@@ -0,0 +1,38 @@
+﻿namespace TgBotPractice.DataBase;
+
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+ 
+public class User
+{
+    public int Id { get; set; }
+    public long ChatId { get; set; }
+    public string City { get; set; }
+    public bool Autosend { get; set; }
+    public bool IsAdmin { get; set; }
+}
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+    public ApplicationContext()
+    {
+        Database.Migrate();
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=test.db");
+    }
+}
+
+public class SampleContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+{
+    public ApplicationContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+        optionsBuilder.UseSqlite("Data Source=test.db");
+        
+        return new ApplicationContext();
+    }
+}
