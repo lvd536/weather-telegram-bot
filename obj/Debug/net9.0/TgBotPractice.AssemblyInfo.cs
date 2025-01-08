320fce8175ff9a52e805cfc9ce94914dff229565
lvd536 <nbikiti@gmail.com>
2025-01-09 01:07:32 +0400

    sort

--- obj/Debug/net9.0/TgBotPractice.AssemblyInfo.cs
+++ obj/Debug/net9.0/TgBotPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TgBotPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fbbdf7f5ecb5fcb4fb22a170c12f75a8250a84b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ddddda2694111d5ba810d68dfbd73d42e372b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TgBotPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TgBotPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
