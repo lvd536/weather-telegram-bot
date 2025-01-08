a73028cb55838121aa26a91cdfb84278567377d0
lvd536 <nbikiti@gmail.com>
2025-01-08 04:37:01 +0400

    simple changes

--- Program.cs
+++ Program.cs
@@ -3,23 +3,20 @@ using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using TgBotPractice;
 
-//3e9eae6efa142dac8de06fd29fffca12
-
 using var cts = new CancellationTokenSource();
 var bot = new TelegramBotClient("7557004382:AAFSqf56fgYQWHvpg1VU6zGJxJ_mdaQnkTI", cancellationToken: cts.Token);
-var me = await bot.GetMe(); // Гетаем нейм бота
+var me = await bot.GetMe();
 WeatherCommand weatherCommand = new WeatherCommand();
 StartCommand startCommand = new StartCommand();
 bot.OnMessage += OnMessage;
 bot.OnUpdate += OnCallbackQuery;
 Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
 Console.ReadLine();
-cts.Cancel(); // stop the bot
+cts.Cancel();
 
-// МСГ Хендл
 async Task OnMessage(Message msg, UpdateType type)
 {
-    if (msg.Text is null) return;	// Если текс нулл - идет нахуй
+    if (msg.Text is null) return;
     if (msg.Text.StartsWith('/'))
     {
         switch (msg.Text)
@@ -33,7 +30,7 @@ async Task OnMessage(Message msg, UpdateType type)
                 break;
         }
     }
-    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}"); // DEBUG data
+    Console.WriteLine($"[Debug] Received {type} '{msg.Text}' in {msg.Chat}");
 }
 
 async Task OnCallbackQuery(Update update)
